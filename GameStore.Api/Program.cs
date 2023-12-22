4e8a54454c318fabd9f5c479c7d40488f64918da
vicktoh <ayomidevictor349@gmail.com>
2023-12-22 15:42:46 +0100

    Cross-origin for front-end

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,11 +1,21 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRepositories(builder.Configuration);
 
+
+builder.Services.AddCors(Options => Options.AddPolicy("localhost:3000",
+    builder => builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .WithMethods("GET", "POST", "PUT", "DELETE")
+));
+
 var app = builder.Build();
 app.Services.InitializeDb();
+
+app.UseCors("localhost:3000");
 
 
 app.MapGamesEndpoints();
